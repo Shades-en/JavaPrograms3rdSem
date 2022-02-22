72df36ca5ca654868c274c42e86ea15d5ec6e3ac
Owais Iqbal <owaisiqbal2013@gmail.com>
2022-02-22 17:54:41 +0530

    update

--- java programs/program11/Student.java
+++
@@ -1,6 +0,0 @@
-package program11;
-
-public interface Student {
-    public void getName();
-    public void getBranch();
-}
--- java programs/program6/StringOp.java
+++ java programs/program6/StringOp.java
@@ -1,6 +1,7 @@
 package program6;
 
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
 
 class StringOp{
     public static void main(String[] args) {
