994cdcda94c9153d91421643894e2b71a0d2306b
Owais Iqbal <owaisiqbal2013@gmail.com>
2022-02-22 17:37:01 +0530

    update

--- java programs/program1/Fibo.java
+++ java programs/program1/Fibo.java
@@ -1,27 +1,34 @@
 package program1;
 
-import java.util.*;
-public class Fibo{
-    void fibonacci(int n){
-        int first = 0;
-        int second = 1; 
-        System.out.println(first);
-        System.out.println(second);
-        n-=2;
-        for(int i=0;i<n;i++){   
-            int third = first + second;
-            System.out.println(third);
-            first=second;
-            second=third;
-        }
+import java.util.Scanner;
+
+public class fibo {
+    int fibonacci(int n){
+        if(n<=1)
+            return n;
+        return fibonacci(n-1)+fibonacci(n-2);
+        
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Fibo fib = new Fibo();
-        System.out.println("Enter the value of n");
-        int n = sc.nextInt();
-        System.out.println("The series is");
-        fib.fibonacci(n);
-        sc.close();
+        fibo fib= new fibo();
+        System.out.println("Enter the number of fibonacci series");
+        int n= sc.nextInt();
+        System.out.println("The series is in recursion is");
+        for(int i=0;i<n;i++)
+            System.out.print(fib.fibonacci(i)+"\t"); 
+        System.out.println();
+        System.out.println("Through loop is");
+        int i=0;
+        int num1=0;
+        int num2=1;
+        while(i<n){
+            System.out.print(num1+"\t");
+            int num3=num1+num2;
+            num1=num2;
+            num2=num3;
+            i++;
+        }
     }
 }
